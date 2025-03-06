e11f6e417864e3cc03a069b55ec3b95d26028348
ardatscn <129105306+ardatscn@users.noreply.github.com>
2025-03-06 21:39:01 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,16 @@
 import streamlit as st
 import os
+from langchain_google_genai import GoogleGenerativeAIEmbeddings
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_community.vectorstores import FAISS
+from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain.chains import create_retrieval_chain
+from langchain.chains.combine_documents import create_stuff_documents_chain
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_community.tools import DuckDuckGoSearchResults
+from elevenlabs.client import ElevenLabs
+from IPython.display import Audio
+
 
 st.title("Hello World")
 

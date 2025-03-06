8fb40c26f1c7f6dc03e0141a6890bed7cbdb9092
ardatscn <129105306+ardatscn@users.noreply.github.com>
2025-03-06 21:37:37 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,10 @@
 import streamlit as st
+import os
 
 st.title("Hello World")
+
+google_api_key = st.secrets.get("GOOGLE_API_KEY")
+elevenlabs_api_key = st.secrets.get("ELEVENLABS_API_KEY")
+
+os.environ["GOOGLE_API_KEY"] = google_api_key
+embeddings =  GoogleGenerativeAIEmbeddings(model="models/text-embedding-004")

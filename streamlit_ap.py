ea3633adce6079835901de4c557845c284c31bd5
ardatscn <129105306+ardatscn@users.noreply.github.com>
2025-03-07 22:34:22 +0300

    Update streamlit_ap.py

--- streamlit_ap.py
+++ streamlit_ap.py
@@ -30,7 +30,7 @@ files = response.json()
 a = 0
 for file in files:
   a += 1
-  if a < 5:
+  if a < 10:
     fname = file['name']
     raw_url = file["download_url"]  # Get the raw URL of the file
     

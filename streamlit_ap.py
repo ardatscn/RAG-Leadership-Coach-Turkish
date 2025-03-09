39f0aa2b139563ff40f748deb6bde391dbd41c3b
ardatscn <129105306+ardatscn@users.noreply.github.com>
2025-03-09 19:10:20 +0300

    Update streamlit_ap.py

--- streamlit_ap.py
+++ streamlit_ap.py
@@ -2,7 +2,7 @@ import streamlit as st
 import os
 from langchain_google_genai import GoogleGenerativeAIEmbeddings
 from langchain.text_splitter import RecursiveCharacterTextSplitter
-from faiss-cpu import FAISS
+from langchain_community.vectorstores import FAISS
 from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain.chains import create_retrieval_chain
 from langchain.chains.combine_documents import create_stuff_documents_chain
